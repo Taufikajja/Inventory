e1055434806854da30ad2722d5c3f3ba4e08b28d
Taufikajja <taufikh.furqon@gmail.com>
2025-08-28 03:46:35 +0700

    tambah

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,24 @@
+
+const add = async (req, res) => {
+    try {
+        const {categoryName, categoryDescription} = req.body;
+        
+        const existingCategory = await Category.findOne({name:categoryName}); 
+        if (existingCategory) {
+            return res.status(400).json({ success: false, message:'Category already exists'});
+        }
+
+        const newCategory = new Category({
+            categoryName,
+            categoryDescription,
+        });
+
+        await newCategory.save();
+        return res.status(201).json({ success:true,message:'Category added successfully', data})
+    } catch (error) {
+        console.error('Error adding category:', error);
+        return res.status(500).json({ success: false, message: "Server error"});
+    }
+}
+
+export {addCategory};
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './db/connection.js';
 import authRoutes from './routes/auth.js';
+import categoryRouter from "./routes/category.js"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('.api/Category')
 
 app.listen(process.env.PORT, () => {
     connectDB();
