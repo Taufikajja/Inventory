f5090eeb35efb4d4c0eb368cfea42ee47a4a20d0
Taufikajja <taufikh.furqon@gmail.com>
2025-08-28 00:32:44 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,62 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import Root from './utils/Root'
+import Login from './pages/Login'
+import ProtectedRoutes from './utils/ProtectedRoutes'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  
+  return (
+    <Router> 
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/admin-dashboard" element={<ProtectedRoutes requireRole={["admin"]}>
+          <Dashboard />
+        </ProtectedRoutes>
+      } 
+      >
+          <Route 
+            index
+            element= {<h1>Summary of dashboard</h1>} 
+          />
+          <Route 
+            path="categories"
+            element={<h1>Categories</h1>}
+          />
+          <Route
+            path="products"
+            element={<h1>Products</h1>}
+          />
+          <Route
+            path="suppliers"
+            element={<h1>Suppliers</h1>}
+          />
+          <Route
+            path="orders"
+            element={<h1>Orders</h1>}
+          />
+          <Route
+            path="users"
+            element={<h1>Users</h1>}
+          />
+          <Route
+            path="profile"
+            element={<h1>Profile</h1>}
+          />
+          <Route
+            path="logout"
+            element={<h1>Logout</h1>}
+          />
+        </Route>
+        <Route path="/customer/dashboard" element={<h1>customer dashboard</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/unauthorized" element={<p className='font-bold text-3xl mt-20 ml-20'>Unauthorized</p>} />
+      </Routes>
+    </Router>
+
+  )
+}
+
+export const useAuth = () => useContext(AuthContext);
+export default App
